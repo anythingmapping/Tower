8b77d5b7a34c4a31305300f8c3d58d24c55bdaac
Lucas <lucas.mostyn@hexagon.com>
2022-08-09 21:05:28 +0200

    working code pause

---
+++ src/placement_tile.js
@@ -0,0 +1,25 @@
+export class Placement_tile {
+    constructor({ position = { x: 0, y: 0 } }) {
+        this.position = position
+        this.size = 64
+        this.color = 'rgba(255,255,255,0.5)';
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.position.x, this.position.y, this.size, this.size);
+    }
+
+    update(mouse) {
+        this.draw();
+
+        if (mouse.x > this.position.x &&
+            mouse.x < this.position.x + this.size &&
+            mouse.y > this.position.y &&
+            mouse.y < this.position.y + this.size) {
+            console.log('a thing!!!!')
+        }
+
+
+    }
+};
